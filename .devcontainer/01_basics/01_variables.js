e3588fae793be25da3db82cd707ec4faf95bd43e
Tirth Mewada <102960937+tirthmewada@users.noreply.github.com>
2024-11-01 06:21:20 +0000

    datatypes

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144533
+let accountEmail ="tirth@gmail.com"
+var accountPassword = "1234567890"
+accountCity = "Jaipur"
+
+//accountId = 2 //Not Allowed
+
+accountEmail = "tirth@clg.com"
+accountPassword = "345534"
+accountCity = "Bengaluru"
+
+
+
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountId,accountPassword])
---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,6 @@
+let name = "tirth"
+let age = 21
+let isLoggedin = false
+let state;
+console.log(name,age)
+console.log(typeof age)
