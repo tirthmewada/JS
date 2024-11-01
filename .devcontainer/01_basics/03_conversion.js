b3f975fe666132a0629a0e0363e3b6b98f5ba777
Tirth Mewada <102960937+tirthmewada@users.noreply.github.com>
2024-11-01 06:35:10 +0000

    change

---
+++ .devcontainer/01_basics/03_conversion.js
@@ -0,0 +1,19 @@
+let score ="33"
+
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let Value = Number(score)
+
+
+console.log(typeof Value);
+console.log(Value);
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
