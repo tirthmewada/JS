d14388877236997144054c153ec7fa3346a721d5
Tirth Mewada <102960937+tirthmewada@users.noreply.github.com>
2024-11-03 16:15:45 +0000

    added content

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -11,3 +11,31 @@ console.log(Arr.at(2));
 
 myArr.push(6)
 console.log(myArr);
+
+const myn1 = myArr.slice(1,4)
+console.log(myn1);
+
+const myn2 = myArr.splice(1,4)
+console.log(myn2);
+
+//Part-2 Array 
+
+const marvel_hero = ["Ironman" , " Thor", "Spiderman"]
+const dc_hero = ["Batman" , "Flash" , "Superman"]
+
+//marvel_hero.push(dc_hero)
+//console.log(marvel_hero);
+
+const allhero = marvel_hero.concat(dc_hero)
+console.log(allhero);
+
+const another_array = [1,2,3,[4,5],6,[7,8,[9]]]
+const real_array = another_array.flat(Infinity)
+console.log(real_array);
+
+let score1 = 100 
+let score2 = 200
+let score3 = 300 
+
+ console.log(Array.of(score1,score2,score3));
+ 
