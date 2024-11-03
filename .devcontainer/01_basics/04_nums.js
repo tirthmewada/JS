34aada2a83758a81bab02556e755f2c3a20dbb18
Tirth Mewada <102960937+tirthmewada@users.noreply.github.com>
2024-11-03 11:25:25 +0000

    Added

---
+++ .devcontainer/01_basics/04_nums.js
@@ -0,0 +1,24 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString.length);
+
+const hundreds = 10000000
+console.log(hundreds.toLocaleString('en-IN'));
+
+///////////////////MAthssss//////////////
+
+console.log(Math);
+
+console.log(Math.random);
+console.log((Math.random()*10)+1);
+
+const min = 10
+const max = 20 
+
+console.log(Math.floor(Math.random() * ( max - min+1))+ min);
+
+
---
+++ .devcontainer/01_basics/05_date.js
@@ -0,0 +1,22 @@
+let myDate = new Date()
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleDateString());
+console.log(typeof myDate);
+
+
+
+let myCreatedDate = new Date(2023 , 3 , 29)
+console.log(myCreatedDate.toLocaleDateString());
+
+
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth());
+console.log(newDate.getUTCDate);
+
+
